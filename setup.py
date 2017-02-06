679bf2094a2ef129e994b9072dfb71419ce780d7
Xavier Mathew <xavier@compile.com>
2017-02-07 00:52:00 +0530

    (fix) missing var breaking setup

--- setup.py
+++ setup.py
@@ -14,9 +14,10 @@ except ImportError:
 install_reqs = parse_requirements('requirements.txt', session=PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
 sys.path.insert(0, os.path.dirname(__file__))
+version = '0.1'
 setup(
     name='agua',
-    version='0.1',
+    version=version,
     packages=find_packages(),
     install_requires=reqs,
     description='Compare data in columns with other columns with the help of comparator functions',
