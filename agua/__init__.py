90030e2715e21533bdec500a7d04629369423de7
Cyriac Thomas <cyriac@compile.com>
2017-03-08 12:49:19 +0530

    replacing click with python-fire

--- agua/__init__.py
+++ agua/__init__.py
@@ -1,4 +1,4 @@
-import click
+import fire
 import unicodecsv as csv
 import yaml
 
@@ -9,88 +9,80 @@ from agua.utils import as_percent, label_width, get_result_filename
 from agua.validators import EMPTY_VALUES
 
 
-@click.group()
-def cli():
-    '''
-    Compare data in columns with other columns with
-    the help of comparator functions
-    '''
+
+class Agua(object):
+    def list_commands(self):
+        '''List built-in check functions'''
+        print('\n'.join(f for f in CHECK_FUNCTIONS))
 
 
-@cli.command('list')
-def list_commands():
-    '''List built-in check functions'''
-    click.echo('\n'.join(f for f in CHECK_FUNCTIONS))
+    def test(self, config="agua.yml",
+             test="", update=True,
+             format_result=True):
+        '''Run tests'''
 
+        try:
+            with open(config) as f:
+                config = yaml.load(f)
+        except IOError:
+            print("Missing config file: %s " % (config))
+            exit()
 
-@cli.command()
-@click.option('--config', default="agua.yml", help='config for tests', required=True, metavar='<path>')
-@click.option('--test', default="", help='file to evaluate', required=True, metavar='<path>')
-@click.option('--update', help='update input file with results', default=True, is_flag=True)
-@click.option('--format_result', help='output 1/0 instead of True/False in result', default=True, is_flag=True)
-def test(config, test, update, format_result):
-    '''Run tests'''
+        fname = config.get('test')
+        if test not in EMPTY_VALUES:
+            fname = test
 
-    try:
-        with open(config) as f:
-            config = yaml.load(f)
-    except IOError:
-        print("Missing config file: %s " % (config))
-        exit()
+        with open(fname) as f:
+            r = csv.DictReader(f)
+            fieldnames = r.fieldnames
+            data = list(r)
 
-    fname = config.get('test')
-    if test not in EMPTY_VALUES:
-        fname = test
+        config = config['config']
 
-    with open(fname) as f:
-        r = csv.DictReader(f)
-        fieldnames = r.fieldnames
-        data = list(r)
+        result = evaluate(data, config)
 
-    config = config['config']
+        total = len(data)
+        print("Test results for %s" % (fname))
+        args = {'width': 50, 'format': '{:>8.2f}', 'suffix': '%', 'verbose': False}
 
-    result = evaluate(data, config)
-
-    total = len(data)
-    print("Test results for %s" % (fname))
-    args = {'width': 50, 'format': '{:>8.2f}', 'suffix': '%', 'verbose': False}
-
-    for i, d in enumerate(result['result']):
-        c = config[i]
-        column = c['base_column']
-        test_column = c.get('test_column', 'test_%s' % column)
-        print(label_width('Column') + ': %s vs %s' % (column, test_column))
-        labels = [label_width('Coverage (%s/%s)' % (d['attempted'], total)),
-                  label_width('Accuracy (%s/%s)' % (d['success'], d['attempted']))]
-        data = [as_percent(d['attempted'], total),
-                as_percent(d['success'], d['attempted'])]
-        data = map(float, data)
-        termchart(labels, data, args)
-
-    if update:
-        updated_fieldnames = list(fieldnames)
-
-        for c in config:
+        for i, d in enumerate(result['result']):
+            c = config[i]
             column = c['base_column']
-            result_column = 'agua_result_%s' % column
-            if result_column not in updated_fieldnames:
-                test_column = c.get('test_column', 'test_%s' % column)
-                updated_fieldnames.insert(
-                    updated_fieldnames.index(test_column) + 1, result_column)
+            test_column = c.get('test_column', 'test_%s' % column)
+            print(label_width('Column') + ': %s vs %s' % (column, test_column))
+            labels = [label_width('Coverage (%s/%s)' % (d['attempted'], total)),
+                      label_width('Accuracy (%s/%s)' % (d['success'], d['attempted']))]
+            data = [as_percent(d['attempted'], total),
+                    as_percent(d['success'], d['attempted'])]
+            data = map(float, data)
+            termchart(labels, data, args)
 
-        new_file = get_result_filename(fname)
-        with open(new_file, 'w') as f:
-            w = csv.DictWriter(f, fieldnames=updated_fieldnames)
-            w.writeheader()
-            for row in result['data']:
-                if format_result:
-                    for c in config:
-                        column = c['base_column']
-                        result_column = 'agua_result_%s' % column
-                        row[result_column] = int(row[result_column]) if row[
-                            result_column] not in EMPTY_VALUES else None
-                w.writerow(row)
+        if update:
+            updated_fieldnames = list(fieldnames)
 
+            for c in config:
+                column = c['base_column']
+                result_column = 'agua_result_%s' % column
+                if result_column not in updated_fieldnames:
+                    test_column = c.get('test_column', 'test_%s' % column)
+                    updated_fieldnames.insert(
+                        updated_fieldnames.index(test_column) + 1, result_column)
+
+            new_file = get_result_filename(fname)
+            with open(new_file, 'w') as f:
+                w = csv.DictWriter(f, fieldnames=updated_fieldnames)
+                w.writeheader()
+                for row in result['data']:
+                    if format_result:
+                        for c in config:
+                            column = c['base_column']
+                            result_column = 'agua_result_%s' % column
+                            row[result_column] = int(row[result_column]) if row[
+                                result_column] not in EMPTY_VALUES else None
+                    w.writerow(row)
+
+def cli():
+    fire.Fire(Agua)
 
 if __name__ == '__main__':
     cli()
